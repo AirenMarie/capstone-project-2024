a97660ebae4c49dda56ea1112abaaa95db726cdf
AirenMarie <astringer92@gmail.com>
2024-12-10 19:12:54 -0500

    remove comments

--- code/script.js
+++ code/script.js
@@ -1,14 +1,3 @@
-// toggle displays //
-// play music, sounds, etc. //
-// set reminder notifications, etc. //
-// use API for reminders and exercises, if necessary //
-// task object(s) with properties (e.g. id, title, dueDate/dueTime, isCompleted) //
-// reminder object(s) with properties (e.g. taskId reminderTime) //
-// functions to add, edit, delete, and mark tasks as completed //
-// system for reminder scheduling (using setTimeout or setInterval) //
-// timers for exercise duration, animation, sound effects //
-// save tasks and reminders to local storage //
-// list major functions //
 const greeting = document.querySelector(".greeting");
 
 const date = newDate();
