15742e3853533970a2761d32879a01b622c5fa57
AirenMarie <astringer92@gmail.com>
2024-12-10 19:37:43 -0500

    remove commented code

--- code/index.js
+++ code/index.js
@@ -1,12 +1,3 @@
-/* const { google } = require("googleapis");
-
-const { OAuth2 } = google.auth;
- */
-/* const oAuth2Client = new OAuth2(
-  "912041195919-b5veekogu6881g0d8nq02acmpc652ta4.apps.googleusercontent.com",
-  "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"
-); */
-
 const CLIENT_ID =
   "912041195919-b5veekogu6881g0d8nq02acmpc652ta4.apps.googleusercontent.com";
 
@@ -14,11 +5,6 @@ const API_KEY = "AIzaSyAcBh7us9iD3_HDfekKYyNa93YyVorSGoY;";
 
 const DISCOVERY_DOC =
   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
-
-/* oAuth2Client.setCredentials({
-  refresh_token:
-    "1//04lg1XvT5oTr7CgYIARAAGAQSNwF-L9IrYX5U28QEJdZZ6MB_0TVkZTT-n1IDEPbTOKyVi8bPPP95CDJmAJlGoQDiURmqYGluEjQ",
-}); */
 
 const SCOPES = "https://www.googleapis.com/auth/calendar";
 
