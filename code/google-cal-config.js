5aaa97b96b4bb28510f0868d86a72a0b7ad9a58a
AirenMarie <astringer92@gmail.com>
2024-12-12 18:49:52 -0500

    update code

--- code/google-cal-config.js
+++ code/google-cal-config.js
@@ -13,6 +13,9 @@ let tokenClient;
 let gapiInited = false;
 let gisInited = false;
 
+document.getElementById("authorize_button").style.visibility = "hidden";
+document.getElementById("signout_button").style.visibility = "hidden";
+
 function gapiLoaded() {
   gapi.load("client", initializeGapiClient);
 }
@@ -23,15 +26,23 @@ async function initializeGapiClient() {
     discoveryDocs: [DISCOVERY_DOC],
   });
   gapiInited = true;
+  maybeEnableButtons();
 }
 
 function gisLoaded() {
   tokenClient = google.accounts.oauth2.initTokenClient({
     client_id: CLIENT_ID,
     scope: SCOPES,
-    callback: "",
+    callback: "", // defined later
   });
   gisInited = true;
+  maybeEnableButtons();
+}
+
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById("authorize_button").style.visibility = "visible";
+  }
 }
 
 function handleAuthClick() {
@@ -39,13 +50,66 @@ function handleAuthClick() {
     if (resp.error !== undefined) {
       throw resp;
     }
+    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("authorize_button").innerText = "Refresh";
+    await listUpcomingEvents();
   };
 
   if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
     tokenClient.requestAccessToken({ prompt: "consent" });
   } else {
-    on.tokenClient.requestAccessToken({ prompt: "" });
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
   }
 }
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken("");
+    document.getElementById("content").innerText = "";
+    document.getElementById("authorize_button").innerText = "Authorize";
+    document.getElementById("signout_button").style.visibility = "hidden";
+  }
+}
+
+async function listUpcomingEvents() {
+  let response;
+  try {
+    const request = {
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 10,
+      orderBy: "startTime",
+    };
+    response = await gapi.client.calendar.events.list(request);
+  } catch (err) {
+    document.getElementById("content").innerText = err.message;
+    return;
+  }
+
+  const events = response.result.items;
+  if (!events || events.length == 0) {
+    document.getElementById("content").innerText = "No events found.";
+    return;
+  }
+
+  const output = events.reduce(
+    (str, event) =>
+      `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+    "Events:\n"
+  );
+  document.getElementById("content").innerText = output;
+}
+
+
 
 redirect.onclick = handleAuthClick();
