aa7ac9fefddd4efde42bcee4e158ba0b7aec272c
AirenMarie <astringer92@gmail.com>
2026-01-15 20:19:56 -0500

    remove files with Google API info

--- code/js/config.js
+++
@@ -1,7 +0,0 @@
-/* const CLIENT_ID =
-  "912041195919-b5veekogu6881g0d8nq02acmpc652ta4.apps.googleusercontent.com";
-
-const API_KEY = "AIzaSyAcBh7us9iD3_HDfekKYyNa93YyVorSGoY";
-
-module.exports = CLIENT_ID;
-module.exports = API_KEY; */
